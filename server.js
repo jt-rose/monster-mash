8b55a0f28e418d40f1afa88ebc2d57a4f3659ded
Jeffrey Rose <jtr219@outlook.com>
2022-02-09 11:00:46 -0500

    set up ejs rendering with public assets

--- server.js
+++ server.js
@@ -10,41 +10,51 @@ const main = async () => {
   app.use(methodOverride("_method"));
   app.use(express.urlencoded({ extended: true }));
 
-  // set up routes
+  /* -------------------------------------------------------------------------- */
+  /*                                set up routes                               */
+  /* -------------------------------------------------------------------------- */
 
   // GET - show all monsters
   app.get("/monster-mash", (req, res) => {
-    res.send("hello");
-  });
-
-  // GET - show individual monster
-  app.get("/monster-mash/:monsterid", (req, res) => {
-    res.send("hello");
+    res.render("index.ejs", {
+      title: "Monster Mash",
+    });
   });
 
   // GET - show monster add form
   app.get("/monster-mash/add", (req, res) => {
-    res.send("hello");
+    res.render("add.ejs", {
+      title: "Add Monster",
+    });
+  });
+
+  // GET - show individual monster
+  app.get("/monster-mash/:monsterid", (req, res) => {
+    res.render("show.ejs", {
+      title: "Show Monster",
+    });
   });
 
   // GET - show monster edit form
   app.get("/monster-mash/edit/:monsterid", (req, res) => {
-    res.send("hello");
+    res.render("edit.ejs", {
+      title: "Edit Monster",
+    });
   });
 
   // POST - add new monster
   app.post("/monster-mash", (req, res) => {
-    res.send("hello");
+    res.redirect("/monster-mash");
   });
 
   // PUT - update monster
   app.put("/monster-mash", (req, res) => {
-    res.send("hello");
+    res.redirect("/monster-mash");
   });
 
   // DELETE - remove monster
   app.delete("/monster-mash", (req, res) => {
-    res.send("hello");
+    res.redirect("/monster-mash");
   });
 
   // start server
