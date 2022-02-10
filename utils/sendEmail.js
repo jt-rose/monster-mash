d3e8c44f2f4a21449a890fd6bf3b16af1df74031
Jeffrey Rose <jtr219@outlook.com>
2022-02-10 11:41:01 -0500

    add sendMail function

---
+++ utils/sendEmail.js
@@ -0,0 +1,31 @@
+const nodemailer = require("nodemailer");
+
+module.exports = async function sendEmail(to, html, user, pass) {
+  //   let testAccount = await nodemailer.createTestAccount();
+  //   console.log("test account", testAccount);
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user,
+      pass,
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Fred Foo 👻" <foo@example.com>',
+    to,
+    subject: "Change Password",
+    html,
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+};
